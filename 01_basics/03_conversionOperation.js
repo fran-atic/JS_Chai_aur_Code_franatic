c71a936c8f051948e3f7881c6fc08de478f5cdd6
fran-atic <117529910+fran-atic@users.noreply.github.com>
2024-10-27 07:00:08 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,26 @@
+let score = "rebanto"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score);
+//console.log(valueInNumber);
+
+// "33" => 33
+// "33abs" => NaN
+// true => 1 ; false => 0
+
+let isLoggedIn = "rebanto"
+let  booleanisLoggedIn = Boolean(isLoggedIn);
+//console.log(booleanisLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "rebanto" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof(stringNumber));
+

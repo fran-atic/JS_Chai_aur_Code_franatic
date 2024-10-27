3923d913d204c1742409ef3c7ee0f87315c4b018
fran-atic <117529910+fran-atic@users.noreply.github.com>
2024-10-27 06:46:56 +0000

    datatypes learnt

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs, not the browser
+
+console.log(3+3)
+console.log("rebanto")
+
+let name = "rebanto"
+let age = 21
+let isLoggedIn = false
+
+// number
+// bigInt
+// string => "",''
+// boolean => true/false
+// null => standalone value
+// undefined => value is not assigned
+// symbol => unique
+
+// object
+
+console.log(typeof undefined);
+console.log(typeof null);
